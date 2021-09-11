1fe9f3bb25ea1ceeedb3c00d374164002c9ea8eb
yangzhuang <koolss@koolss.com>
2021-09-12 00:10:42 +0800

    顶部图片

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -99,6 +99,7 @@ export default class Index extends Component {
     let {showQuestionModel, questionList} = this.state;
     return (
       <View className='index'>
+        <Image className='top-img' src="https://img11.360buyimg.com/babel/s700x360_jfs/t1/4776/39/2280/143162/5b9642a5E83bcda10/d93064343eb12276.jpg!q90!cc_350x180" mode="scaleToFill"/>
         <View className='title'>问答模块案例</View>
         <View className='question-list'>
           {
